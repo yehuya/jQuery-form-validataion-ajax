0d39944fedb2a4b992fd027f0fe213234daac7ad
yehuya <test@test.test>
2017-12-26 12:31:13 +0200

    refactoring but we still used jQuery for $.ajax

--- formValidationAjax.js
+++ formValidationAjax.js
@@ -2,30 +2,31 @@
 
 /**
  * send form via ajax with validation
- * jquery
+ * we used jquery for $.ajax only
  */
 
-(function($){
+(function(){
     // JQUERY IS MUUST
-    if(typeof $ === 'undefined') return console.error('jQuery is must for sendForm plugin');
+    if(typeof jQuery === 'undefined') return console.error('jQuery is must for sendForm plugin');
 
-    // DOM binding
-    $.fn.form = function(options){
-        return new form(this, options);
+    if(typeof jQuery !== undefined){
+        // DOM binding
+        jQuery.fn.form = function(options){
+            return new form(this, options);
+        }
     }
 
     function form(jqueryElem, options){
         this.elem = jqueryElem;
 
-        var option = {
+        this.options = extendImmutable({
             url: null,
             method: null,
             crossDomain: true,
             dataType: 'jsonp',
             done: null
-        }
-
-        this.options = $.extend(option, options);
+        }, options);
+        
         this.inputs = [];
         this.ok;
         this.click = true; // for prevent dbl sending
@@ -42,7 +43,7 @@
      * prevent browser validation
      */
     form.prototype.noBrowserValidation = function(){
-        this.elem.attr('novalidate', 'novalidate');
+        this.elem.setAttribute('novalidate', 'novalidate');
     }
 
     /**
@@ -51,30 +52,13 @@
      * @retun Object of Array
      */
     form.prototype.getFields = function(){
-        var field = {};
-        var input = this.elem.find('input');
-        var select = this.elem.find('select');
-        var textarea = this.elem.find('textarea');
+        this.field = {
+            input: [].slice.call(this.elem.getElementsByTagName('input')),
+            select: [].slice.call(this.elem.getElementsByTagName('select')),
+            textarea: [].slice.call(this.elem.getElementsByTagName('textarea'))
+        };
         
-        var inputClean = [];
-        for(var i = 0 ; i < input.length ; i++){
-            inputClean.push(input[i]);
-        }
-        field.input = inputClean;
-
-        var selectClean = [];
-        for(var i = 0 ; i < select.length ; i++){
-            selectClean.push(select[i]);
-        }
-        field.select = selectClean;
-
-        var textareaClean = [];
-        for(var i = 0 ; i < textarea.length ; i++){
-            textareaClean.push(textarea[i]);
-        }
-        field.textarea = textareaClean;
-        
-        return this.fields = field;
+        return this.fields;
     }
 
     /**
@@ -82,54 +66,26 @@
      */
     form.prototype.validation = {
         israelPhone: function(value){
-            var rgx = /^(02|03|04|08|09|05\d|072|073|075|076|077|078)[\-\s{1}]?\d{1}[\-\s{1}]?\d{6}$/;
-            var valid = rgx.test(value);
-            
-            if(valid){
-                return true;
-            }else{
-                return false;
-            }
+            var rgx = /^(02|03|04|08|09|05\d|072|073|075|076|077|078)[\-\s{1}]?\d{1}[\-\s{1}]?\d{6}$/;  
+            return rgx.test(value);
         },
         fullName: function(value){
             var split = value.trim().split(' ');
-            if(split.length > 1){
-                var valid = true;
-                for(var i = 0 ; i < split.length ; i++){
-                    valid = this.text(split[i]);
-                    if(!valid){
-                        return valid;
-                    }
-                }
+            if(split <= 1) return false;
 
-                return valid;
-            }else{
-                return false;
+            for(var i = 0 ; i < split.length ; i++){
+                if(!this.text(split[i])) return false;
             }
+
+            return true;
         },
         text: function(value){
             var rgx = /[!@#\$%\^&\*\[\]:;'"<>|+\\]|\d/;
-            var valid = rgx.test(value);
-
-            if(!valid){
-                if(value.trim().length > 1){
-                    return true;
-                }else{
-                    return false;
-                }
-            }else{
-                return false;
-            }
+            return (!rgx.test(value) && value.trim().length > 1);
         },
         email: function(value){
             var rgx =  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            var valid = rgx.test(value);
-            
-            if(valid){
-                return true;
-            }else{
-                return false;
-            }
+            return rgx.test(value);
         }
     }
 
@@ -141,23 +97,17 @@
         var fields = this.fields;
        
         for(var f in fields){
-            for(var i = 0 ; i < fields[f].length ; i++){
-                var field = fields[f][i];
-              
-                if(field && field.name != undefined && field.name.trim().length > 0 && field.type != 'submit'){
-                    var obj = {
-                        name: field.name,
-                        type: f == 'input' ? field.type : f,
-                        value: field.value,
-                        require: field.hasAttribute('required') ? true : false,
-                        validation: field.hasAttribute('validation') ? field.getAttribute('validation') : false,
-                        validation_text: field.hasAttribute('validation-text') ? field.getAttribute('validation-text') : false,
-                        element: field
-                    }
-
-                    this.inputs.push(obj);
+            this.inputs.concat(fields[f].map(function(field){
+                return {
+                    name: field.name,
+                    type: f == 'input' ? field.type : f,
+                    value: field.value,
+                    require: field.hasAttribute('required') ? true : false,
+                    validation: field.hasAttribute('validation') ? field.getAttribute('validation') : false,
+                    validation_text: field.hasAttribute('validation-text') ? field.getAttribute('validation-text') : false,
+                    element: field
                 }
-            }
+            }));
         }
 
         return this.inputs; 
@@ -167,27 +117,20 @@
      * check if input are valid
      * @return Array of Object (update this.inputs)
      */
-    form.prototype.inputsValid = function(){
-        var inputs = this.inputs;
-        
-        for(var i = 0 ; i < inputs.length ; i++){
-            var input = inputs[i];
+    form.prototype.inputsValid = function(){        
+        for(var i = 0 ; i < this.inputs.length ; i++){
+            var input = this.inputs[i];
             if(input.element) input.value = input.element.value; // get the current value
+
             if(input.require){
-                if(input.validation){
-                    var valid = this.validation[input.validation](input.value);
-                    input.valid = valid; 
+                if(input.validation) {
+                    input.valid = this.validation[input.validation](input.value);
                 }else{
-                    if(input.value.trim().length > 0){
-                        input.valid = true;
-                    }else{
-                        input.valid = false;
-                    }
+                    input.valid = input.value.trim().length > 0;
                 }
             }else{
                 if(input.validation && input.value.trim().length > 0){
-                    var valid = this.validation[input.validation](input.value);
-                    input.valid = valid; 
+                    input.valid = this.validation[input.validation](input.value);
                 }else{
                     input.valid = true;
                 }
@@ -196,41 +139,47 @@
     }
 
     /**
-     * if input not valid - form not will send
+     * show / hide error on input
+     * @param Object 
+     */
+    form.prototype.setValidError = function(input){
+        if(!input.element.classList.contains('inputError')){
+            input.element.classList.add('inputError');
+        }
+
+        var placeholder = input.validation_text;
+        var oldVal = input.element.value;
+
+        if(input.element.hasAttribute('placeholder')){
+            input.element.value = '';
+            if(placeholder) input.element.setAttribute('placeholder', placeholder);
+
+            var focus = function(){
+                input.element.value = oldVal;
+                input.element.removeEventListener('focus', focus);
+            }
+
+            input.element.addEventListener('focus', focus);
+        }
+    }
+
+    /**
+     * if input not valid - form not send
      * add element error class
      * add element placeholder error text
      */
     form.prototype.valid = function(){
         var inputs = this.inputs;
-        var setValidError = function(input){
-            if(!input.element.classList.contains('inputError')){
-                input.element.classList.add('inputError');
-            }
-
-            var placeholder = input.validation_text;
-            var oldVal = input.element.value;
-
-            if(input.element.hasAttribute('placeholder')){
-                input.element.value = '';
-                if(placeholder) input.element.setAttribute('placeholder', placeholder);
-
-                var focus = function(){
-                    input.element.value = oldVal;
-                    input.element.removeEventListener('focus', focus);
-                }
-
-                input.element.addEventListener('focus', focus);
-            }
-        }
         
         this.ok = true;
         for(var i = 0 ; i < inputs.length ; i++){
             if(!inputs[i].valid){ 
-                setValidError(inputs[i]);
+                this.setValidError(inputs[i]);
                 this.ok = false;
-            }else{
-                if(inputs[i].element) inputs[i].element.classList.remove('inputError');
+                continue;
             }
+            
+            if(inputs[i].element) inputs[i].element.classList.remove('inputError');
         }
     }
 
@@ -239,17 +188,14 @@
      * @param Object
      */
     form.prototype.addParam = function(obj){
-        var def = {
+        var param = extendImmutable({
             name: null,
             value: null,
             require: false,
             validation: false
-        }
+        }, obj);
 
-        var param = $.extend(def, obj);
-        if(param.name != null){
-            this.inputs.push(param);
-        }
+        if(param.name) this.inputs.push(param);
 
         return this;
     }
@@ -269,7 +215,7 @@
      * set object for ajax
      */
     form.prototype.ajaxObjects = function(){
-        var ajax = {}
+        var ajax = {};
         var inputs = this.inputs;
 
         for(var i = 0 ; i < inputs.length ; i++){
@@ -285,7 +231,7 @@
     form.prototype.submit = function(){
         var self = this;
 
-        this.elem.on('submit', function(e){
+        this.elem.addEventListener('submit', function(e){
             e.preventDefault();
             self.inputsValid();
             self.ajaxObjects();
@@ -306,4 +252,13 @@
         });
     }
 
-})(jQuery);
+    function extendImmutable(source, target){
+        var res = {};
+        for(var f in source){
+            res[f] = target.hasOwnProperty(f) ? target[f] : source[f];
+        }
+    
+        return res;
+    }
+
+})();
